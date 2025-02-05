69f42f4eaa9829fc2b808e11d322d23a4b413382
sourc3c0de <costapintosjm@gmail.com>
2025-02-05 13:56:13 +0000

    Actualización

--- w2w/settings.py
+++ w2w/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l^^yda*ia(+**gv0!t41u6(=xx71^&=94ci$+2izz*_%-l^f+g'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
