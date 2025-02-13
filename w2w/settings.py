38dbca5dbf6bf5a99901e58c748161d2bfde069a
electrosolucion2025 <electrosolucion2025@gmail.com>
2025-02-13 18:56:23 +0000

    Switch to production settings and update payment link configuration
    - Revert payment link to production domain
    - Change DEBUG to False
    - Switch database configuration to use dj_database_url
    - Remove commented-out database and link configurations

--- w2w/settings.py
+++ w2w/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -110,20 +110,20 @@ WSGI_APPLICATION = 'w2w.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', 5432),
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST', 'localhost'),
+#         'PORT': os.getenv('DB_PORT', 5432),
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -166,7 +166,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",  # Backend local
     "https://whats2want.up.railway.app",  # Dominio en producción
 ]
-
 
 CSRF_COOKIE_SECURE = True  # No requiere HTTPS en desarrollo
 CSRF_COOKIE_HTTPONLY = True  # Permitir acceso desde el frontend
