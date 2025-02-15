fc0118238554df01c8ab0b2dd1d8a458899a2fce
electrosolucion2025 <electrosolucion2025@gmail.com>
2025-02-15 13:32:33 +0000

    Fix import path for Tenant model in printers views
    Update import statement to use the correct app-specific path for the Tenant model

--- apps/printers/views.py
+++ apps/printers/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.timezone import now
 from apps.printers.models import PrintTicket
-from tenants.models import Tenant
+from apps.tenants.models import Tenant
 
 @csrf_exempt
 def get_tickets_for_printing(request):
